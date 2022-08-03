9d682f35966bed7d9ca38642f0362e47ef3f9360
Njato007 <njatotianafiononana@gmail.com>
2022-08-03 15:01:53 +0300

    Animated page

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,11 @@ window.addEventListener('scroll', () => {
   if (this.scrollY > 90) {
       header_top.classList.add('hidden');
       header_middle.classList.add('fixed-top');
+      document.querySelector('.body').style.marginTop = '90px';
   } else {
       header_top.classList.replace('hidden', 'showing');
       header_middle.classList.remove('fixed-top');
+      document.querySelector('.body').style.marginTop = '0px';
   }
   
   var current = "";
@@ -15,6 +17,14 @@ window.addEventListener('scroll', () => {
     const sectionTop = section.offsetTop;
     if (pageYOffset + 1 >= sectionTop)
       current = section.getAttribute("id");
+    if (current === 'about')
+      aboutAnimation();
+    else if (current === 'skill')
+      skillAnimation();
+    else if (current === 'project')
+      projectAnimation();
+      else if (current === 'contact')
+        contactAnimation();
   });
 
   var navLi = document.querySelectorAll('a[href^="#"]');
@@ -25,3 +35,72 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+const aboutAnimation = () => {
+  let title = document.querySelector('.profile-section > h4.section-title');
+  title.style.animationName = 'come-from-left';
+  title.style.animationDelay = '0s';
+  // image
+  let img = document.querySelector('.my-pic > img');
+  img.style.animationName = 'turn-verticaly';
+  // info
+  let info = document.querySelector('.my-info-content');
+  info.style.animationName = 'bottom-to-top';
+}
+
+const skillAnimation = () => {
+  let title = document.querySelector('.skill-section > h4.section-title');
+  title.style.animationName = 'come-from-right';
+  title.style.animationDelay = '0s';
+  // skill title
+  let skill_title = document.querySelector('.skill-title');
+  skill_title.style.animationName = 'fadeIn';
+  // skill pl logo
+  let img_item = document.querySelectorAll('.prog-lang-item');
+  let delay = 0.5;
+  for (let item of img_item) {
+    item.style.animationDelay = `${delay}s`;
+    item.style.animationName = 'fadeIn';
+    delay += 0.2;
+  }
+}
+
+const projectAnimation = () => {
+  let title = document.querySelector('.project-section > h4.section-title');
+  title.style.animationName = 'come-from-left';
+  title.style.animationDelay = '0s';
+  // project item
+  let project_item = document.querySelectorAll('.project-item');
+  for (let item of project_item) {
+    item.style.animationName = 'fadeIn';
+  }
+  // let description
+  let desc = document.querySelectorAll('.project-desc');
+  for (let item of desc) {
+    item.style.animationName = 'go-to-top';
+  }
+  console.log(desc.length)
+  // img
+  let img_item = document.querySelectorAll('.project-view > img');
+  for (let item of img_item) {
+    item.style.animationName = 'grow-from-center';
+  }
+  // link
+  let links = document.querySelectorAll('.project-link');
+  for (let item of links) {
+    item.style.animationName = 'go-to-bottom';
+  }
+}
+
+const contactAnimation = () => {
+  let title = document.querySelector('.contact-section > h4.section-title');
+  title.style.animationName = 'come-from-right';
+  title.style.animationDelay = '0s';
+  let form_title = document.querySelector('.my-form > .form-title');
+  form_title.style.animationName = 'fadeIn';
+  // project item
+  let inputs = document.querySelectorAll('.form-group');
+  inputs.forEach((item, idx) => {
+    item.style.animationName = idx % 2 === 0 ? 'come-from-left' : 'come-from-right';
+  });
+}
--- public/js/toast.js
+++ public/js/toast.js
@@ -1,34 +1,32 @@
+const Toast = (options = { position: 'alert-top-center', color: 'alert-dark', message: ''}) => {
+  var div1 = document.createElement('div');
+  div1.className = `alert ${options.position} ${options.color}`;
 
-const Toast = (options = {
-    position: 'alert-top-center',
-    color: 'alert-dark',
-    message: ''
-  }) => {
-    var div1 = document.createElement('div');
-    div1.className = `alert ${options.position} ${options.color}`;
-    var div2 = document.createElement('div');
-    div2.className = 'alert-icons';
-    var bell = document.createElement('i');
-    bell.className = 'fa fa-';
-    var times = document.createElement('i');
-    times.className = 'fa fa-times alert-dismiss';
-    times.onclick = () => div1.remove();
-    var p = document.createElement('p');
-    p.className = 'alert-message';
-    div2.append(bell);
-    div2.append(times);
-    var text = document.createTextNode(options.message);
-    div1.append(div2);
-    p.append(text);
-    div1.append(p);
-    // append to the body page
-    document.body.append(div1);
-    
+  var div2 = document.createElement('div');
+  div2.className = 'alert-icons';
+
+  var bell = document.createElement('i');
+  bell.className = 'fa fa-';
+  var times = document.createElement('i');
+  times.className = 'fa fa-times alert-dismiss';
+  times.onclick = () => div1.remove();
+  var p = document.createElement('p');
+  p.className = 'alert-message';
+  
+  div2.append(bell);
+  div2.append(times);
+  var text = document.createTextNode(options.message);
+  div1.append(div2);
+  p.append(text);
+  div1.append(p);
+  // append to the body page
+  document.body.append(div1);
+  
+  setTimeout(() => {
+    div1.style.animationName = 'fadeOut';
     setTimeout(() => {
-      div1.style.animationName = 'fadeOut';
-      setTimeout(() => {
-        div1.remove();
-      }, 1500);
-    }, 5000);
+      div1.remove();
+    }, 1500);
+  }, 5000);
 }
   
